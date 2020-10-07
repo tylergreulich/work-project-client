4997298563033c39a9b051b777009fd95f3afd4e
tylergreulich <tgreul6@gmail.com>
2020-10-07 13:59:15 -0700

    updating loading text on mount

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Routes } from "./Routes";
 import { setAccessToken } from "./accessToken";
+import styled from 'styled-components'
+
+const LoadingWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+`
 
 export const App = () => {
   const [loading, setLoading] = useState(true);
@@ -17,7 +26,7 @@ export const App = () => {
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <LoadingWrapper>Loading...</LoadingWrapper>;
   }
 
   return <Routes />;
